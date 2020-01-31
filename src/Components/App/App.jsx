c9656a687b9968536706b41e9ffbafd7b38f8406
Barbosa <reginaldobr@gmail.com>
2020-01-31 12:39:42 -0300

    bootstrap working

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+
+import "./App.css";
 // import logo from './logo.svg';
-import Index from "../Index/Index"
-import Dashboard from "../Dashboard/Dashboard"
+import Index from "../Index/Index";
+import Dashboard from "../Dashboard/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 class App extends Component {
@@ -11,16 +16,21 @@ class App extends Component {
     return (
       <div className="App">
         <Switch>
-          <Route exact path="/" component={Index}/>
-          <Route path="/dashboard" component={Dashboard}/>
+          <Route exact path="/" component={Index} />
+          <Route path="/dashboard" component={Dashboard} />
+        {/* <Link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        /> */}
         </Switch>
-          
+
       </div>
     );
   }
 }
 
 export default App;
-
 
 //TODO IMPORTAR bootstrap CSS e JAVASCRIPT
