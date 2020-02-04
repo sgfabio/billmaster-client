133f5edee412b2e00602deed5a2b28655db154d8
Barbosa <reginaldobr@gmail.com>
2020-02-04 17:57:35 -0300

    dashboard expenses page

--- src/Components/DashDespesas/Checkbox.jsx
+++ src/Components/DashDespesas/Checkbox.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
 export const CheckBox = props => {
+  // {props.doSomething(props.isChecked)}
   return (
     <li className="d-flex mx-auto dropdown-item ">
-      <input className="mx=auto my-auto" key={props.id} id={props.id} onClick={props.handleCheckChieldElement} type="checkbox" checked={props.isChecked} name={props.name} />
-      <label className="dropdown-item mx-0 my-auto bg-transparent" for={props.id} > {props.name} </label>
+      <input
+        className="mx=auto my-auto"
+        // key="split"
+        id={props.name}
+        onClick={props.handleCheckChieldElement}
+        type="checkbox"
+        checked={props.isChecked}
+        name="divideBy"
+        value={props.name}
+        onChange={props.handleChange}
+        />
+      <label
+        className="dropdown-item mx-0 my-0 bg-transparent"
+        for={props.name}
+      >
+        {props.name}
+      </label>
     </li>
   );
 };
